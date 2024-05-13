2370a08ccc9f1d41f665033b3ad14bed24a7d291
Akshay <akshayjee6@gmail.com>
2024-05-13 14:05:58 +0530

    basic setup is completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Navbar, Container } from "react-bootstrap";
+import Form from "./components/form/Form";
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
